dd40bf7ac97b6763be2f83aa430fc1d1988f3711
jjlmoya <juanjo1989@gmail.com>
2026-07-17 09:33:52 +0200

    chore: tests and export adaptation

--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,6 @@
 export { nauticalCategory } from './category';
 export type { CategoryLocaleContent } from './category';
 
-export { tideCalculator } from './tool/tideCalculator';
-export { underKeel } from './tool/underKeel';
-export { nauticalConverter } from './tool/nauticalConverter';
-export { sailArea } from './tool/sailArea';
-export { speedConverter } from './tool/speedConverter';
-export { endurance } from './tool/endurance';
 
 export type { TideCalculatorUI, TideCalculatorLocaleContent } from './tool/tideCalculator';
 export type { UnderKeelUI, UnderKeelLocaleContent } from './tool/underKeel';
